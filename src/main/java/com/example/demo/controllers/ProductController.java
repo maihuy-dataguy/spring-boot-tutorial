31cd4884f99630308f8fa7ee8c4a645ef0266305
maihuy12121999 <sofm12121999>
2023-06-18 19:03:21 +0700

    Response object update

--- src/main/java/com/example/demo/controllers/ProductController.java
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -2,12 +2,13 @@ package com.example.demo.controllers;
 
 
 import com.example.demo.models.Product;
+import com.example.demo.models.ResponseObject;
 import com.example.demo.repositories.ProductRepository;
+import org.apache.logging.log4j.message.ReusableMessage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
@@ -29,9 +30,26 @@ public class ProductController {
     //Get specific product
     @GetMapping("/{id}")
     //Let 's return an object with :data, message, status
-    Product getProductById(@PathVariable Long id){
-        return repository.findById(id).orElseThrow(
-                ()->new RuntimeException("Cannot find product with id = "+ id)
+    ResponseEntity<ResponseObject> getProductById(@PathVariable Long id){
+        Optional<Product> foundProduct  = repository.findById(id);
+
+        return foundProduct.isPresent()?
+                ResponseEntity.status(HttpStatus.OK).body(
+                        new ResponseObject("ok","Query product successfully",foundProduct)
+                ):
+                ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                        new ResponseObject("failed","Cannot find product with id = " + id,"")
+                );
+//        return repository.findById(id).orElseThrow(
+//                ()->new RuntimeException("Cannot find product with id = "+ id)
+//        );
+    }
+
+    //insert new Product with Post method
+    ResponseEntity<ResponseObject> insertProduct (@RequestBody Product newProduct){
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject("ok","Insert product succesfully", repository.save(newProduct))
         );
     }
+
 }
